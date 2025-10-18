5830cb1e94e63f1aa5fd954f20ec36e38e118381
魏曹先生 <1992414357@qq.com>
2025-10-18 18:23:15 +0800

    feat: add new binary files and utilities
    - Add jvref.rs binary
    - Add build_env_logger.rs utility module

---
+++ src/bin/jvref.rs
@@ -0,0 +1,7 @@
+// Import i18n files
+rust_i18n::i18n!("locales", fallback = "en");
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, World!")
+}
---
+++ src/utils/build_env_logger.rs
@@ -0,0 +1,66 @@
+use std::path::Path;
+
+use colored::Colorize;
+use env_logger::{Builder, Target};
+use log::{Level, LevelFilter};
+
+pub fn build_env_logger(log_path: impl AsRef<Path>) {
+    use std::io::{self, Write};
+
+    struct MultiWriter<A, B> {
+        a: A,
+        b: B,
+    }
+
+    impl<A: Write, B: Write> MultiWriter<A, B> {
+        fn new(a: A, b: B) -> Self {
+            Self { a, b }
+        }
+    }
+
+    impl<A: Write, B: Write> Write for MultiWriter<A, B> {
+        fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+            let _ = self.a.write(buf);
+            self.b.write(buf)
+        }
+
+        fn flush(&mut self) -> io::Result<()> {
+            let _ = self.a.flush();
+            self.b.flush()
+        }
+    }
+
+    let mut builder = Builder::new();
+
+    let log_format = |buf: &mut env_logger::fmt::Formatter, record: &log::Record| {
+        let now = chrono::Local::now();
+
+        let level_style = match record.level() {
+            Level::Error => record.args().to_string().red().bold(),
+            Level::Warn => record.args().to_string().yellow().bold(),
+            Level::Info => record.args().to_string().white(),
+            Level::Debug => record.args().to_string().white(),
+            Level::Trace => record.args().to_string().cyan(),
+        };
+
+        writeln!(
+            buf,
+            "{} {}",
+            now.format("%H:%M:%S")
+                .to_string()
+                .truecolor(105, 105, 105)
+                .bold(),
+            level_style
+        )
+    };
+
+    let log_file = std::fs::File::create(log_path).expect("Failed to create log file");
+    let combined_target = Target::Pipe(Box::new(MultiWriter::new(std::io::stdout(), log_file)));
+
+    builder
+        .format(log_format)
+        .filter(None, LevelFilter::Info)
+        .filter_module("just_enough_vcs", LevelFilter::Trace)
+        .target(combined_target)
+        .init();
+}
