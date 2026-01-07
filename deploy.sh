21cee9242d37f4acd31dd90cf5c82763d216c873
魏曹先生 <1992414357@qq.com>
2026-01-08 03:49:08 +0800

    Fix path to compile_info.rs template

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ if FORCE_BUILD=$(date +%s) cargo build --workspace --release; then
     # Export
     if cargo run --manifest-path crates/build_helper/Cargo.toml --bin exporter; then
         # Copy compile_info.rs.template to compile_info.rs after successful export
-        cp -f src/data/compile_info.rs.template src/data/compile_info.rs
+        cp -f templates/compile_info.rs src/data/compile_info.rs
     fi
 fi
