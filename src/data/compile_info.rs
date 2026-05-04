027987c8bc497f93e7f1e3bc4b880f53b229fd04
魏曹先生 <1992414357@qq.com>
2026-05-04 14:53:55 +0800

    Replace hardcoded compile info with placeholders

--- src/data/compile_info.rs
+++ src/data/compile_info.rs
@@ -13,13 +13,13 @@ pub struct CompileInfo {
 impl Default for CompileInfo {
     fn default() -> Self {
         Self {
-            date: "2026-03-26 16:24:44".to_string(),
-            target: "x86_64-unknown-linux-gnu".to_string(),
-            platform: "Linux".to_string(),
-            toolchain: "rustc 1.91.1 (ed61e7d7e 2025-11-07) (stable)".to_string(),
-            cli_version: "0.1.1".to_string(),
-            build_branch: "dev".to_string(),
-            build_commit: "96e19f1b28e9ad3a58864b41a9d6e25ed255dac6".to_string(),
+            date: "<<<date>>>".to_string(),
+            target: "<<<target>>>".to_string(),
+            platform: "<<<platform>>>".to_string(),
+            toolchain: "<<<toolchain>>>".to_string(),
+            cli_version: "<<<version>>>".to_string(),
+            build_branch: "<<<branch>>>".to_string(),
+            build_commit: "<<<commit>>>".to_string(),
         }
     }
 }
